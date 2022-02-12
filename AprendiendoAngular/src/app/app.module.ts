5de5a077ae8a4107070d3d43800ed0dc0d10dd93
Francisco Larrosa <franciscolarrosa22@gmail.com>
2022-02-12 20:49:42 -0300

    add new component by command

--- AprendiendoAngular/src/app/app.module.ts
+++ AprendiendoAngular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { miComponente } from './components/miComponente/miComponente.component';
+import { MoviesComponent } from './components/movies/movies.component';
 @NgModule({
   declarations: [
     AppComponent,
-    miComponente
+    miComponente,
+    MoviesComponent
   ],
   imports: [
     BrowserModule
