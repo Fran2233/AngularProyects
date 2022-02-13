1bd0ea4497251bf440ad2588475f77564a67a101
Francisco Larrosa <franciscolarrosa22@gmail.com>
2022-02-13 10:13:45 -0300

    cambios

--- AprendiendoAngular2/src/app/app.component.ts
+++ AprendiendoAngular2/src/app/app.component.ts
@@ -9,7 +9,8 @@ export class AppComponent {
 nombre = 'Fran'
 texto = 'Escriba algo';
 deshabilitado = true;
-
+imgSrc = 'https://w7.pngwing.com/pngs/549/955/png-transparent-angularjs-javascript-jquery-directive-python-software-foundation-angle-triangle-logo.png';
+texto2 = 'Event Binding';
 
 constructor(){
   setInterval(()=>this.deshabilitado = false,3000)
@@ -17,5 +18,9 @@ constructor(){
 
 getSuma(num1: number,num2: number){
   return num1 +num2;
+}
+
+cambiarTexto():void{
+  this.texto2 = 'Event Binding 2 a partir de un metodo en el button';
 }
 }
