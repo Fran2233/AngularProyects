5ba954d740076add88183ad1a0e4d4115072d75a
Francisco Larrosa <franciscolarrosa22@gmail.com>
2022-02-12 23:14:17 -0300

    cambios

--- AprendiendoAngular2/src/app/app.component.ts
+++ AprendiendoAngular2/src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 nombre = 'Fran'
+texto = 'Escriba algo';
+deshabilitado = true;
+
+
+constructor(){
+  setInterval(()=>this.deshabilitado = false,3000)
+}
+
+getSuma(num1: number,num2: number){
+  return num1 +num2;
+}
 }
