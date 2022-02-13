b3d58c958dd87a4ddd4430e2b0a34527c470fae4
Francisco Larrosa <franciscolarrosa22@gmail.com>
2022-02-13 10:31:24 -0300

    cambios

--- AprendiendoAngular2/src/app/app.component.ts
+++ AprendiendoAngular2/src/app/app.component.ts
@@ -6,21 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-nombre = 'Fran'
-texto = 'Escriba algo';
-deshabilitado = true;
-imgSrc = 'https://w7.pngwing.com/pngs/549/955/png-transparent-angularjs-javascript-jquery-directive-python-software-foundation-angle-triangle-logo.png';
-texto2 = 'Event Binding';
+  nombre = 'Fran'
+  texto = 'Escriba algo';
+  deshabilitado = true;
+  imgSrc = 'https://w7.pngwing.com/pngs/549/955/png-transparent-angularjs-javascript-jquery-directive-python-software-foundation-angle-triangle-logo.png';
+  texto2 = 'Event Binding';
+  texto3 = '';
 
-constructor(){
-  setInterval(()=>this.deshabilitado = false,3000)
-}
+  constructor() {
+    setInterval(() => this.deshabilitado = false, 3000)
+  }
 
-getSuma(num1: number,num2: number){
-  return num1 +num2;
-}
+  getSuma(num1: number, num2: number) {
+    return num1 + num2;
+  }
 
-cambiarTexto():void{
-  this.texto2 = 'Event Binding 2 a partir de un metodo en el button';
-}
+  cambiarTexto(): void {
+    this.texto2 = 'Event Binding 2 a partir de un metodo en el button';
+  }
+
+  listEstudiantes: any[] = [
+    {
+      nombre: 'Fran',
+      estado: 'Aprobado'
+    },
+    {
+      nombre: 'Pedro',
+      estado: 'Promoción'
+    },
+    {
+      nombre: 'Juan',
+      estado: 'Reprobado'
+    }
+  ]
 }
