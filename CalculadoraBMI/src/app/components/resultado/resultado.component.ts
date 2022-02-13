618d0fbaff8422c0ef607381a7c7f64df15f26c9
Francisco Larrosa <franciscolarrosa22@gmail.com>
2022-02-13 19:54:00 -0300

    BMI-calculator-finish

--- CalculadoraBMI/src/app/components/resultado/resultado.component.ts
+++ CalculadoraBMI/src/app/components/resultado/resultado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-resultado',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resultado.component.css']
 })
 export class ResultadoComponent implements OnInit {
+  BMI: number;
+  resultado: string;
+  interpretacion: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { 
+    this.resultado='';
+    this.interpretacion='';
+    this.BMI = +route.snapshot.paramMap.get('valor')!;
+  }
 
   ngOnInit(): void {
+    this.getResultado();
+  }
+
+  getResultado(){
+    if(this.BMI >= 25){
+      this.resultado = 'Exceso de peso';
+      this.interpretacion = 'Tienes un peso corporal superior al normal , procura cambiar tu estilo de vida';
+    }else if(this.BMI >= 18.5){
+      this.resultado = 'Normal';
+      this.interpretacion = 'Tienes un peso normal. ¡Buen Trabajo!';
+    } else{
+      this.resultado = 'Bajo Peso';
+      this.interpretacion = 'Tienes un peso inferior a lo normal, ve a un nutricionista';
+    }
   }
 
 }
