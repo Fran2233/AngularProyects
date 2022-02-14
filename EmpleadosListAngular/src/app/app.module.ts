eb1b67304517513e16b15aa5e2214308014a918c
Francisco Larrosa <franciscolarrosa22@gmail.com>
2022-02-13 22:15:00 -0300

    proyect-ListEmpleados

---
+++ EmpleadosListAngular/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { EmpleadosListComponent } from './components/empleados-list/empleados-list.component';
+import { CountEmpleadosComponent } from './components/empleadosList/count-empleados/count-empleados.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmpleadosListComponent,
+    CountEmpleadosComponent,
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
